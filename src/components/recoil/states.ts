952b315223672518cbf7477c2f0cdd9a2f921248
tty-hash <hashi.ndig@gmail.com>
2022-11-10 18:40:46 +0900

    ref #3 firebase からデータを取得して、recoil で状態管理できるようにした。firebase から直接 recoil に入れるとpromise のため上手く行かなかったので、result を recoil へセットして state として登録した。

--- src/components/recoil/states.ts
+++ src/components/recoil/states.ts
@@ -1,6 +1,15 @@
 import { atom } from "recoil";
+type GoodsList = {
+  goodsUrl: string;
+  goodsCarbo: string;
+  goodsFat: string;
+  goodsProtein: string;
+  goodsCalorie: string;
+  goodsValue: string;
+  goodsTitle: string;
+};
 
-export const goodsList = atom<any>({
+export const goodsList = atom<GoodsList[]>({
   key: "goodsList", //一意の値
   default: [], //初期値
 });
