613df338bf02bc334851341927ad9a624764e4f2
tty-hash <hashi.ndig@gmail.com>
2022-11-17 07:42:35 +0900

    ref #9 カートボタンクリックで GoodsId を recoil の state へ保持、追加できるようにした。

--- src/hooks/useGoodsAddCart.tsx
+++ src/hooks/useGoodsAddCart.tsx
@@ -1,9 +1,12 @@
 import React from 'react'
+import { useSetRecoilState } from 'recoil'
+import { cart } from '../components/recoil/states'
 
-const useGoodsAddCart = () => {
-  return (
-    <div>useGoodsAddCart</div>
-  )
+/**
+ * 商品のドキュメントidを取得して State へ保持
+ * @param {string} goodsId 
+ */
+export const useGoodsAddCart = (goodsId:string) => {
+  const setCartState = useSetRecoilState(cart);
+  setCartState(prev => [...prev,goodsId]);
 }
-
-export default useGoodsAddCart
