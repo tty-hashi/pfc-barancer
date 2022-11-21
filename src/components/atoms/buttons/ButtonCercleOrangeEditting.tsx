10993b02cd250485f6020ff6f773edcb092cf197
tty-hash <hashi.ndig@gmail.com>
2022-11-22 06:31:54 +0900

    ref #14 my-page の edit ボタンクリックでメニューの編集できる機能を実装した。

--- src/components/atoms/buttons/ButtonCercleOrangeEditting.tsx
+++ src/components/atoms/buttons/ButtonCercleOrangeEditting.tsx
@@ -2,9 +2,30 @@ import React from "react";
 import { faPenToSquare } from "@fortawesome/free-solid-svg-icons";
 
 import ButtonCercleOrange from "./ButtonCercleOrange";
+import { useSetRecoilState } from "recoil";
+import { cart } from "../../recoil/states";
+import { useRouter } from "next/router";
 
-const ButtonCercleOrangeEditting = () => {
-  return <ButtonCercleOrange icon={faPenToSquare} />;
+type Props = {
+  goodsDetailId: string[];
+  menuName: string;
+};
+
+const ButtonCercleOrangeEditting: React.FC<Props> = (props) => {
+  const { goodsDetailId, menuName } = props;
+  const setGoodsInCart = useSetRecoilState(cart);
+  const router = useRouter();
+
+  /**
+   * Mymenu の中の商品の id を配列で cart の state へ渡し、top へ遷移する
+   * @return {void}
+   */
+  const editingMymenu: () => void = () => {
+    if (!confirm(`${menuName}を元に新たなメニューを作成します。`)) return;
+    setGoodsInCart(goodsDetailId);
+    router.push("/");
+  };
+  return <ButtonCercleOrange icon={faPenToSquare} onClick={editingMymenu} />;
 };
 
 export default ButtonCercleOrangeEditting;
