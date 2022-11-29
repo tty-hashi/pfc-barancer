20d20d146821187686996b7c066ecdadd56bddc5
tty-hash <hashi.ndig@gmail.com>
2022-11-30 07:56:53 +0900

    ref #20 スタイリングを調整した。

--- src/components/molecules/BottomFixedCalorie.tsx
+++ src/components/molecules/BottomFixedCalorie.tsx
@@ -13,7 +13,7 @@ const BottomFixedCalorie = () => {
   const uid = useRecoilValue(userIdState);
 
   return (
-    <Box position="fixed" bottom="0" left="0" w="100%" bg="#fff" zIndex='sticky'>
+    <Box position="fixed" bottom="0" left="0" w="100%" bg="#fff" zIndex='sticky' boxShadow=' 0 0 3px #eee'>
       <Box margin="0 auto" py={{ base: 2, md: 4 }} maxW="700px" display="flex" justifyContent="space-around" alignItems="center">
         <Box>
           <Text textAlign="center">{calorie}kcal</Text>
